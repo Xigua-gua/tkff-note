889c9a9bdd8671205adb43283f52ac9b7f7ca385
Xigua-gua <646293529@qq.com>
2017-08-26 17:08:17 +0800

    tkff笔记

---
+++ Jquery笔记.js
@@ -0,0 +1,175 @@
+/*
+
+
+*/
+
+所有 jQuery 函数位于一个 document ready 函数中：
+这是为了防止文档在完全加载（就绪）之前运行 jQuery 代码
+通常会把 jQuery 代码放到 <head>部分的事件处理方法中
+<html>
+<head>
+<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+
+--- jQuery functions go here ----
+
+});
+</script>
+</head>
+
+<body>
+<h2>This is a heading</h2>
+<p>This is a paragraph.</p>
+<p>This is another paragraph.</p>
+<button>Click me</button>
+</body>
+
+</html>
+
+jQuery 名称冲突
+jQuery 使用 $ 符号作为 jQuery 的简介方式。
+某些其他 JavaScript 库中的函数（比如 Prototype）同样使用 $ 符号。
+jQuery 使用名为 noConflict() 的方法来解决该问题。
+var jq=jQuery.noConflict()，帮助您使用自己的名称（比如 jq）来代替 $ 符号。
+
+
+prev() 获得匹配元素集合中每个元素紧邻的前一个同胞元素，通过选择器进行筛选是可选的。
+next() 获得匹配元素集合中每个元素紧邻的同胞元素。如果提供选择器，则取回匹配该选择器
+的下一个同胞元素。
+
+
+jQuery CSS 选择器
+jQuery CSS 选择器可用于改变 HTML 元素的 CSS 属性。
+下面的例子把所有 p 元素的背景颜色更改为红色：
+实例
+$("p").css("background-color","red");
+
+jQuery 属性选择器
+jQuery 使用 XPath 表达式来选择带有给定属性的元素。
+$("[href]") 选取所有带有 href 属性的元素。
+$("[href='#']") 选取所有带有 href 值等于 "#" 的元素。
+$("[href!='#']") 选取所有带有 href 值不等于 "#" 的元素。
+$("[href$='.jpg']") 选取所有 href 值以 ".jpg" 结尾的元素。
+
+*	$("*")	所有元素
+选择器	         实例	        选取
+:input	     $(":input")	所有 <input> 元素
+:text	     $(":text")	    所有 type="text" 的 <input> 元素
+:password	 $(":password")	所有 type="password" 的 <input> 元素
+:radio	     $(":radio")  	所有 type="radio" 的 <input> 元素
+:checkbox	 $(":checkbox")	所有 type="checkbox" 的 <input> 元素
+:submit	     $(":submit")   所有 type="submit" 的 <input> 元素
+:reset	     $(":reset")	所有 type="reset" 的 <input> 元素
+:button	     $(":button")	所有 type="button" 的 <input> 元素
+:image	     $(":image")	所有 type="image" 的 <input> 元素
+:file	     $(":file")	所有 type="file" 的 <input> 元素
+
+:first	     $("p:first")	第一个 <p> 元素
+:last	     $("p:last")	最后一个 <p> 元素
+:even	     $("tr:even")	所有 偶数 <tr> 元素
+:odd	     $("tr:odd")	所有 奇数 <tr> 元素
+:header      $(":header")	所有标题元素 <h1> - <h6>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+eq 概述  eq 概述 eq 概述 :获取第N个元素
+/获取匹配的第二个元素   eq(index) 的用法  参数index描述 和 参数-index描述: eq(-index)
+//HTML 代码:
+<p> This is just a test.</p> <p> So is this</p>
+//jQuery 代码:
+$("p").eq(1)               $("p").eq(-2)
+//结果:
+[ <p> So is this</p> ]    [ <p> So is this</p> ]
+
+方法.eq( index )将匹配元素集合缩减为集合中指定位置的元素；方法.first()将匹配元素集合
+缩减为集合中的第一个元素；方法.last()将匹配元素集合缩减为集合中的最后一个元素；
+方法.slice( start [, end] )将匹配元素集合缩减为指定范围的子集。
+方法.first()和.last()通过调用.eq( index )实现，.eq( index )则通过
+.slice( start [, end] )实现，.slice( start [, end] )则通过调用
+.pushStack( elements, name, arguments )实现，
+方法调用链为.first/last()→.eq( index )→.slice( start [, end] )→
+.pushStack( elements, name, arguments )。相关代码如下所示：
+284      eq: function( i ) {
+285         i = +i;
+286         return i === -1 ?
+287             this.slice( i ) :
+288             this.slice( i, i + 1 );
+289      },
+290
+291      first: function() {
+292         return this.eq( 0 );
+293      },
+294
+295      last: function() {
+296         return this.eq( -1 );
+297      },
+298
+299      slice: function() {
+300         return this.pushStack( slice.apply( this, arguments ),
+301             "slice", slice.call(arguments).join(",") );
+302     },
+
+第285行：如果参数i是字符串，则通过在前面加上一个加号把该参数转换为数值。
+
+第300～301行：先借用数组方法slice()从当前jQuery对象中获取指定范围的子集（数组），
+再调用方法.pushStack()把子集转换为jQuery对象，同时通过属性prevObject保留了对当前
+jQuery对象的引用。
+/*
+
+
+
+
+
+
+
+
+
+*/
---
+++ tkff笔记.js
@@ -0,0 +1,148 @@
+
+
+
+log 大法 log 大法 log 大法
+log 大法 :
+var log = function() {
+    console.log.apply(console, arguments)
+}
+
+// 定义我们用于测试的函数
+// ensure 接受两个参数
+// condition 是 bool, 如果为 false, 则输出 message
+// 否则, 不做任何处理
+var ensure = function(condition, message) {
+    // 在条件不成立的时候, 输出 message
+    if(!condition) {
+        log('*** 测试失败:', message)
+    }
+}
+JSON加强版 测试  JSON加强版 测试 JSON加强版 测试
+var arrayEquals = function(a, b, message) {
+    if (JSON.stringify(a) != JSON.stringify(b)) {
+        log(`${message}测试失败---> ${a}不等于${b} `)
+    }
+    if (JSON.stringify(a) === JSON.stringify(b)) {
+        log(`${message}测试成功---> ${a}等于${b}`)
+    }
+}
+
+函数执行遇到 return 就结束 函数执行遇到 return 就结束
+var minus = function(a, b) {
+    return a - b
+    log('这一句是不会被执行的, 因为 return 的时候函数就结束了')
+}
+
+
+Math 标准库函数 Math 标准库函数 Math 标准库函数
+Math.random()   得到一个随机的小数(0 - 1 之间)
+Math.floor(x)	返回 <= x 整数 向下取整
+//Math.floor(1.5) == 1
+Math.ceil( x )  返回 >= x 整数 向下上取整
+Math.round( x )  四舍五入
+Math.abs( x )    取绝对值
+Math.max(1,5,3,8)  取最大值
+Math.min(1,5,3,8)  去最小值
+var max = Math.max.apply(null,array)
+
+
+
+数组方法 数组方法 数组方法
+常用数组方法:
+array.push(a,b,c) 在数组尾部添加元素
+array.pop()   删除一个数组尾部元素
+array.unshift(x,y,z)  在数组头部添加元素
+array.shift()  删除一个数组头部元素
+var arr = [1,2,3]
+arr.join('&') // 返回 '1&2&3'
+
+字符串方法 字符串方法 字符串方法
+字符串方法:
+var str = 'abcdefg'
+str.split('d')// 返回 ['abc', 'efg']
+如何将字符串 按给定长度平均分割 保存到数组
+var ffge = function(str, n) {
+    var array = []
+    var len = str.length / n
+    for (var i = 0; i < len; i++) {
+        var a = str.slice(n*i, n*(i+1))
+        array.push(a)
+    }
+    return array
+}
+
+
+// 内置的基本数据有以下几种类型
+// number          数字
+// string          字符串
+// boolean         布尔变量（只有两个值 true 或 false）
+// object          对象, 是高级一点的内容
+var a
+a = 1       // a 是 number
+a = 1.1     // number
+a = 'good'  // string
+
+typeof( object )	判断 类型
+String( number )	数字 转 字符串
+Number( string )	字符串 转 数字
+
+可以用 typeof 语句得到一个变量的类型
+a = 10
+b = true
+c = 'I am good'
+log('type a', typeof a)
+log('type b', typeof b)
+log('type c', typeof c)
+// 运行, 输出如下
+// type a number
+// type b boolean
+// type c string
+
+Object.prototype.toString.call([1,2]) // "[object Array]"
+typeof([1,2]) //"object"  这是什么鬼
+
+
+typeof a !== 'undefined'
+上面代码会检测 foo 是否已经定义；如果没有定义而直接使用会导致 ReferenceError 的异常。
+这是 typeof 唯一有用的地方。
+结论
+为了检测一个对象的类型，强烈推荐使用 Object.prototype.toString 方法；因为这是唯一一个
+可依赖的方式。正如上面表格所示，typeof 的一些返回值在标准文档中并未定义，因此不同的引擎
+实现可能不同。
+除非为了检测一个变量是否已经定义，我们应尽量避免使用 typeof 操作符。
+
+
+
+
+
+
+
+JSON  JSON  JSON
+var s = JSON.stringify([1, 2, 3, 4])
+log('序列化后的字符串', typeof s, s)
+var a = JSON.parse(s)
+log('反序列化后的数组', typeof a, a)
+
+
+时间函数 时间函数 时间函数 时间函数
+var currentTime = function() {
+    var d = new Date()
+    var month = d.getMonth() + 1
+    var date = d.getDate()
+    var hours = d.getHours()
+    var minutes = d.getMinutes()
+    var seconds = d.getSeconds()
+    var timeString = `${month}/${date} ${hours}:${minutes}:${seconds}`
+    return timeString
+}
+
+omg为父元素,html 为子元素,在父元素中插入子元素用 insertAdjacentHTML
+omg.insertAdjacentHTML('beforeend', html )
+删除元素
+omg.remove( )
+
+insertAdjacentHTML	标签 描述
+beforebegin  	   开始标记 前
+afterbegin	       开始标记 后
+beforeend	       结束标记 前
+afterend	       结束标记 后
